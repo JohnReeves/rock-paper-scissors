41b21fa824fa839363c6bd1c6d78f4ae8dd40689
John Reeves <johnreeves.mathsandcomputing@gmail.com>
2020-02-09 10:47:11 +0000

    what about draws?

--- take-two.py
+++ take-two.py
@@ -8,21 +8,20 @@ paper vs scissor->scissor wins
 """
 
 choices="""
-"Enter choice:
+Enter choice:
 1. Rock
 2. paper
 3. scissor
 """
 
 elements=["rock","paper","scissors"]
-play_again="yes"
 
 print(rules)
-
-hello=input("what is your name? ")
-print(hello)
-
-while play_again in ["y","Y","yes","Yes"]:
+# input() fails for strings in repl.it
+# play_again="yes"
+# while play_again in ["y","Y","yes","Yes"]:
+play_again=1
+while play_again == 1:
     print(choices)
 
     user_choice = int(input("Your turn: "))
@@ -37,30 +36,29 @@ while play_again in ["y","Y","yes","Yes"]:
       comp_choice = random.randint(1, 3) -1
 
     print("Computer choice is: %s " % elements[comp_choice]) 
-
     print("%s vs %s" % (elements[user_choice],elements[comp_choice]))
 
+    # paper & rock
     if((user_choice == 1 and comp_choice == 2) or
        (user_choice == 2 and comp_choice ==1 )): 
-        # print("paper wins => ", end = "") 
         print("paper wins => ") 
         result = elements[1] # "paper"
+    # scissors & rock
     elif((user_choice == 1 and comp_choice == 3) or
          (user_choice == 3 and comp_choice == 1)): 
-        # print("Rock wins =>", end = "") 
         print("Rock wins =>") 
         result = elements[0] # "rock"
+    # scissors & paper?
     else: 
-        # print("scissor wins =>", end = "") 
         print("scissor wins =>") 
         result = elements[2] # "scissors"
+    # what about the draw?
 
     if result == elements[user_choice]: 
         print("<== User wins ==>") 
     else: 
         print("<== Computer wins ==>") 
           
-    # print("Do you want to play again? (Y/N)") 
-    play_again = input("Do you want to play again? (Y/N) ")
-  
+    print("Do you want to play again?") 
+    play_again = int(input("1 for yes; 2 for no: "))
 
